c000b3a2870efc78e68e4107537b10956ab3f06c
Yied Pozi <yiedpozi@gmail.com>
2024-07-09 01:04:45 +0800

    fix: add powered by url

--- config/uitmtimetable.php
+++ config/uitmtimetable.php
@@ -2,6 +2,8 @@
 
 return [
 
+    'powered_by_url'     => env('UITM_TIMETABLE_POWERED_BY_URL', 'https://yiedpozi.my'),
+
     'icress_url'         => env('UITM_TIMETABLE_ICRESS_URL', 'https://simsweb4.uitm.edu.my/estudent/class_timetable/'),
     'icress_referer_url' => env('UITM_TIMETABLE_ICRESS_REFERER_URL', 'https://simsweb4.uitm.edu.my/estudent/class_timetable/index.htm'),
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\TelegramBotController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect()->away(config('uitmtimetable.powered_by_url'));
+    // return view('index');
 });
